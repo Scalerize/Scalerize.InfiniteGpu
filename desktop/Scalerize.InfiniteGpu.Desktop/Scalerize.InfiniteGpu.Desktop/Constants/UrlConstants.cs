9f97497d9a8720e2b066b7360869dbd92f5a39bc
Frank <frank.libolt@hotmail.com>
2025-10-01 19:46:22 +0200

    fix(desktop): reverted uri changes

--- desktop/Scalerize.InfiniteGpu.Desktop/Scalerize.InfiniteGpu.Desktop/Constants/UrlConstants.cs
+++ desktop/Scalerize.InfiniteGpu.Desktop/Scalerize.InfiniteGpu.Desktop/Constants/UrlConstants.cs
@@ -11,12 +11,12 @@ namespace Scalerize.InfiniteGpu.Desktop.Constants
         /// <summary>
         /// Base URI for the backend API in development mode.
         /// </summary>
-        public static readonly Uri BackendBaseUri = new("https://infinite-gpu-backend-bvh8a7c3fdgxd7c5.canadacentral-01.azurewebsites.net/");
+        public static readonly Uri BackendBaseUri = new("http://localhost:5116/");
 
         /// <summary>
         /// URI for the frontend application in development mode.
         /// </summary>
-        public static readonly Uri FrontendUri = new("https://salmon-island-06e155e0f.2.azurestaticapps.net");
+        public static readonly Uri FrontendUri = new("http://localhost:5173");
 #else
         /// <summary>
         /// Base URI for the backend API in production mode.
